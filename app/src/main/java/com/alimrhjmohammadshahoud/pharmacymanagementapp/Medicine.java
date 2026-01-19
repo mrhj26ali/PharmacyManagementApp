4cd9a8d081715aec995810961472f80123d95e04
Ali Mrhj <mrhj26ali@gmail.com>
2026-01-19 20:35:53 +0300

    Add Company and Medicine model classes

---
+++ app/src/main/java/com/alimrhjmohammadshahoud/pharmacymanagementapp/Medicine.java
@@ -0,0 +1,39 @@
+package com.alimrhjmohammadshahoud.pharmacymanagementapp;
+
+public class Medicine {
+
+    private int id;
+    private String name;
+    private int companyId;
+    private int quantity;
+    private double price;
+
+    public Medicine(int id, String name, int companyId, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.companyId = companyId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
