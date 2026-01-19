02fe8d88683be8d0ebf5511e20c0aadc5881ee96
mohamadshahoud <mohamadshahood31@gmail.com>
2026-01-19 22:21:02 +0300

    Add Main Screen

--- app/src/main/java/com/alimrhjmohammadshahoud/pharmacymanagementapp/MainActivity.java
+++ app/src/main/java/com/alimrhjmohammadshahoud/pharmacymanagementapp/MainActivity.java
@@ -1,20 +1,61 @@
 package com.alimrhjmohammadshahoud.pharmacymanagementapp;
 
+import android.content.Intent; // ضروري للانتقال بين الشاشات
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar; // استخدام النسخة المتوافقة
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // تعريف المتغيرات
+    private Button button_ManageInventory;
+    private Button button_InitiateSale;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        setupClickListeners();
+        handleWindowInsets();
+    }
+
+    private void initializeViews() {
+        button_ManageInventory = findViewById(R.id.button_manage_inventory);
+        button_InitiateSale = findViewById(R.id.button_initiate_sale);
+        toolbar = findViewById(R.id.topAppBar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    private void setupClickListeners() {
+        button_ManageInventory.setOnClickListener(mng -> manageInventory());
+        button_InitiateSale.setOnClickListener(initiate -> initiateSale());
+    }
+
+    private void manageInventory() {
+        /// /// activity will be added later
+        Intent intent = new Intent(MainActivity.this, CompanyListActivity.class);
+        startActivity(intent);
+    }
+
+    private void initiateSale() {
+        /// /// activity will be added later
+        Intent intent = new Intent(MainActivity.this, SaleActivity.class);
+        startActivity(intent);
+    }
+
+    private void handleWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
